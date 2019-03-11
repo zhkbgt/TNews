6daee64ca21be135e6ec33991e01b836e9330f29
zh神夕 <kkbbggtt@163.com>
2019-03-11 17:46:55 +0800

    0.1.3
    Signed-off-by: zh神夕 <kkbbggtt@163.com>

---
+++ TNews.framework/Headers/lza01xwmk_vbfyuVC.h
@@ -0,0 +1,19 @@
+//
+//  lza01xwmk_vbfyuVC.h
+//  BGTNews
+//
+//  Created by 张兵 on 2019/2/28.
+//  Copyright © 2019 张兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <TAPI/TAPI.h>
+#import <TAPI/APIWindowContainer.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface lza01xwmk_vbfyuVC : APIWindowContainer
+
+@end
+
+NS_ASSUME_NONNULL_END
